c94fad86b5a917f5c88732b2455c304d03b25055
Dias <diasmashikovnasa@gmail.com>
2022-09-03 20:04:29 -0700

    Bug fix

--- app/src/main/java/com/dmsoftware/calculator/MainActivity.kt
+++ app/src/main/java/com/dmsoftware/calculator/MainActivity.kt
@@ -10,34 +10,22 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.dmsoftware.calculator.ui.theme.CalculatorTheme
+import com.dmsoftware.calculator.ui.theme.MaterialCalculatorTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CalculatorTheme {
+            MaterialCalculatorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CalculatorTheme {
-        Greeting("Android")
     }
 }
